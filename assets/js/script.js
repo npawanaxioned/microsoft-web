fc8c7093d97ef54f97c389df83aaa536f5f4bf13
npawanaxioned <pawann@axioned.com>
2023-06-09 10:36:26 +0530

    hamburger fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const headerNav = document.querySelector(".header-nav-links");
-const navLink = document.querySelectorAll(".nav-link")
+const navLink = document.querySelectorAll(".nav-link");
+const body = document.querySelector("body")
 
 //add active
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
-  headerNav.style.opacity = "1";
-  headerNav.style.height = "auto";
+  headerNav.classList.toggle("active");
 })
 
 //remove active
@@ -14,8 +14,6 @@ navLink.forEach(e => {
   e.addEventListener("click", () => {
     hamburger.classList.remove("active");
     headerNav.classList.remove("active");
-    // headerNav.style.opacity = "0";
-    // headerNav.style.height = "0";
   })
 });
 
