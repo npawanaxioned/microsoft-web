7a76da6951219f145e2a832e36a52bfeac1b0e3b
npawanaxioned <134269965+npawanaxioned@users.noreply.github.com>
2023-06-11 01:52:24 +0530

    hamburger close on body scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const headerNav = document.querySelector(".header-nav-links");
 const navLink = document.querySelectorAll(".nav-link");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 
 //add active
 hamburger.addEventListener("click", () => {
@@ -15,6 +15,11 @@ navLink.forEach(e => {
     hamburger.classList.remove("active");
     headerNav.classList.remove("active");
   })
+});
+
+body.addEventListener("scroll", () =>{
+  hamburger.classList.remove("active");
+  headerNav.classList.remove("active");
 });
 
 
